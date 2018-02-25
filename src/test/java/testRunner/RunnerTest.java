27c149dbe83c7bf5f64bc6626002e904279bc0c7
diegoalonsonunez <diego96danh@gmail.com>
2018-02-25 15:36:31 -0500

    final changes

--- src/test/java/testRunner/RunnerTest.java
+++ src/test/java/testRunner/RunnerTest.java
@@ -1,7 +1,10 @@
 package testRunner;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
@@ -11,12 +14,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class RunnerTest extends AbstractTestNGCucumberTests {
 	public static WebDriver driver;
+	@BeforeMethod
 	@BeforeTest
 	public void setUp(){
 		//option to choose any webDriver 
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		}
+	System.out.println("Runnig script");	
+	}
+	@AfterMethod
+	@AfterTest
+	public void tearDown(){
+		driver.quit();
+	}
+
 		
-	
 }//class
